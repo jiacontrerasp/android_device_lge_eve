a29eba3c6627bec43f5fc33c6bd06228fb611e88
Won-Kyu Park <wkpark@gmail.com>
2011-05-05 13:38:57 +0900

    cleanup and add some proprietary files for video encode/decode
    Change-Id: I06515fd10527e3544667c2cffe431f6190454b0a

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -43,33 +43,6 @@ LOCAL_MODULE_TAGS := eng
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := libOmxH264Dec.so
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
-LOCAL_SRC_FILES := proprietary/$(LOCAL_MODULE)
-OVERRIDE_BUILT_MODULE_PATH := $(TARGET_OUT_INTERMEDIATE_LIBRARIES)
-LOCAL_MODULE_TAGS := eng
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := libOmxMpeg4Dec.so
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
-LOCAL_SRC_FILES := proprietary/$(LOCAL_MODULE)
-OVERRIDE_BUILT_MODULE_PATH := $(TARGET_OUT_INTERMEDIATE_LIBRARIES)
-LOCAL_MODULE_TAGS := eng
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := libOmxVidEnc.so
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
-LOCAL_SRC_FILES := proprietary/$(LOCAL_MODULE)
-OVERRIDE_BUILT_MODULE_PATH := $(TARGET_OUT_INTERMEDIATE_LIBRARIES)
-LOCAL_MODULE_TAGS := eng
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE := libcommondefs.so
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
@@ -117,32 +90,33 @@ OVERRIDE_BUILT_MODULE_PATH := $(TARGET_OUT_INTERMEDIATE_LIBRARIES)
 LOCAL_MODULE_TAGS := eng
 include $(BUILD_PREBUILT)
 
-#$(LOCAL_PATH)/proprietary/libGLES_qcom.so:system/lib/egl/libGLES_qcom.so
 PRODUCT_COPY_FILES += \
-    frameworks/base/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+	frameworks/base/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
 	frameworks/base/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
-    frameworks/base/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
-    frameworks/base/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
-    frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
-    frameworks/base/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
-    frameworks/base/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+	frameworks/base/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+	frameworks/base/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+	frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+	frameworks/base/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+	frameworks/base/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
 	packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:/system/etc/permissions/android.software.live_wallpaper.xml \
-    device/lg/eve/prebuilt/15checkgapps:system/etc/init.d/15checkgapps \
-	device/lg/eve/prebuilt/eve_qwerty-qwertz.kl:system/usr/keylayout/eve_qwerty-qwertz.kl \
-	device/lg/eve/prebuilt/eve_qwerty-azerty.kl:system/usr/keylayout/eve_qwerty-azerty.kl \
+	device/lg/eve/prebuilt/15checkgapps:system/etc/init.d/15checkgapps \
 	device/lg/eve/prebuilt/eve_qwerty.kl:system/usr/keylayout/eve_qwerty.kl \
+	device/lg/eve/prebuilt/7k_handset.kl:system/usr/keylayout/7k_handset.kl \
 	device/lg/eve/prebuilt/qwerty.kl:system/usr/keylayout/qwerty.kl \
-    device/lg/eve/prebuilt/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
+	device/lg/eve/prebuilt/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
 	device/lg/eve/prebuilt/dhcpcd.conf:system/etc/dhcpcd/dhcpcd.conf \
 	device/lg/eve/prebuilt/gps.conf:system/etc/gps.conf
 
 #Radio
-PROPRIETARY := lib/liblgdrmwbxml.so lib/liblgdrmxyssl.so lib/libdll.so lib/libril-qcril-hook-oem.so lib/libgsdi_exp.so lib/libgstk_exp.so lib/libwms.so \
-               lib/libnv.so lib/libwmsts.so lib/liblgeat.so lib/libril_log.so lib/liblgerft.so lib/libbcmwl.so lib/liblgdrm.so lib/libwmdrmpd.so \
-               lib/liboem_rapi.so lib/libdss.so lib/libqmi.so lib/libmmgsdilib.so lib/libcm.so lib/liboncrpc.so lib/libdsm.so lib/libqueue.so \
-			   lib/libril-qc-1.so lib/libdiag.so bin/qmuxd
+PROPRIETARY := lib/liblgdrmwbxml.so lib/liblgdrmxyssl.so lib/libdll.so \
+	lib/libril-qcril-hook-oem.so lib/libgsdi_exp.so lib/libgstk_exp.so lib/libwms.so \
+	lib/libnv.so lib/libwmsts.so lib/liblgeat.so lib/libril_log.so lib/liblgerft.so \
+	lib/libbcmwl.so lib/liblgdrm.so lib/libwmdrmpd.so \
+	lib/liboem_rapi.so lib/libdss.so lib/libqmi.so lib/libmmgsdilib.so \
+	lib/libcm.so lib/liboncrpc.so lib/libdsm.so lib/libqueue.so \
+	lib/libril-qc-1.so lib/libdiag.so bin/qmuxd
 #Wifi
-PROPRIETARY += etc/wl/rtecdc.bin etc/wl/nvram.txt
+PROPRIETARY += etc/wl/rtecdc.bin etc/wl/rtecdc-apsta.bin etc/wl/nvram.txt
 
 #Bluetooth
 PROPRIETARY += bin/BCM4325D0_004.001.007.0168.0169.hcd bin/btld
@@ -150,11 +124,34 @@ PROPRIETARY += bin/BCM4325D0_004.001.007.0168.0169.hcd bin/btld
 #OpenGL
 PROPRIETARY += lib/egl/libGLES_qcom.so
 
-#Video
-PROPRIETARY += lib/libOmxWmvDec.so \
-			   lib/libomx_aacdec_sharedlibrary.so lib/libomx_amrdec_sharedlibrary.so lib/libomx_amrenc_sharedlibrary.so lib/libomx_avcdec_sharedlibrary.so \
-			   lib/libomx_m4vdec_sharedlibrary.so lib/libomx_mp3dec_sharedlibrary.so \
-			   lib/libaomx_mp3dec_sharedlibrary.so lib/libaomx_mp4dec_sharedlibrary.so lib/libaomx_wmadec_sharedlibrary.so lib/libaomx_wmvdec_sharedlibrary.so
+#Video decoder/encoder
+PROPRIETARY += \
+	lib/libOmxMpeg4Dec.so \
+	lib/libOmxH264Dec.so \
+	lib/libOmxWmvDec.so \
+	lib/libOmxVidEnc.so
+
+#qcom OmxCore for encoder/decoder
+PROPRIETARY += \
+	lib/libOmxCore.so \
+	lib/libmm-omxcore.so
+
+#OpenCore decoder/encoder (not used)
+PROPRIETARY += \
+	lib/libomx_aacdec_sharedlibrary.so lib/libomx_amrdec_sharedlibrary.so \
+	lib/libomx_amrenc_sharedlibrary.so lib/libomx_avcdec_sharedlibrary.so \
+	lib/libomx_m4vdec_sharedlibrary.so lib/libomx_mp3dec_sharedlibrary.so
+
+#Arcsoft extension OpenCore decoder/encoder (not used)
+PROPRIETARY += \
+	lib/libaomx_mp3dec_sharedlibrary.so lib/libaomx_mp4dec_sharedlibrary.so \
+	lib/libaomx_wmadec_sharedlibrary.so lib/libaomx_wmvdec_sharedlibrary.so
+
+# qcom Omx
+PROPRIETARY += lib/libqcomm_omx.so
+
+#Camera
+PROPRIETARY += lib/libmmipl.so
 
 #Sensors
 PROPRIETARY += bin/akmd2
